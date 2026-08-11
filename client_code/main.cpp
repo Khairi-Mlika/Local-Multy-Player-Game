4d3bf705c284774cdddf048ef12015151c0cc263
Khairi Mlika <versuskhairi@gmail.com>
2026-08-11 18:18:50 +0100

    version 0.0.0 : connection between client and server (local)

---
+++ client_code/main.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+
+#include <winsock2.h>
+#include <ws2tcpip.h>
+
+int main()
+{
+    int result;
+
+    // init
+    WSAData wsaData;
+    result = WSAStartup(MAKEWORD(2, 2), &wsaData);
+    if (result != NO_ERROR)
+    {
+        std::cout << "WINSOCK::INIT::ERROR" << std::endl;
+        return 1;
+    }
+
+    // creating the socket
+    SOCKET mySock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (mySock == INVALID_SOCKET)
+    {
+        std::cout << "SCOKET::CREATION::ERROR" << std::endl;
+        WSACleanup();
+        return 1;
+    }
+
+    // creating the server address
+    sockaddr_in serverAddr{};
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(8080);
+    inet_pton(AF_INET, "127.0.0.1", &serverAddr.sin_addr);
+
+    // connect to the server
+    result = connect(mySock, (sockaddr *)&serverAddr, sizeof(serverAddr));
+    if (result == SOCKET_ERROR)
+    {
+        std::cout << "CONNECTING::ERROR" << std::endl;
+        WSACleanup();
+        return 1;
+    }
+
+    // sending to the server
+    std::string message = "ks 5alit ooredoo";
+
+    send(mySock, message.c_str(), message.length(), 0);
+
+    closesocket(mySock);
+    WSACleanup();
+
+    return 0;
+}
---
+++ server_code/main.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+
+#include <winsock2.h>
+#include <ws2tcpip.h>
+
+int main()
+{
+    int result = 0;
+
+    // init the winsock library
+    WSADATA wsaData;
+    result = WSAStartup(MAKEWORD(2, 2), &wsaData);
+    if (result != NO_ERROR)
+    {
+        std::cout << "WINSOCK::INIT::ERROR" << std::endl;
+        return 1;
+    }
+
+    // creating the address of our server
+    sockaddr_in serverAddr{};
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(8080);
+    serverAddr.sin_addr.s_addr = INADDR_ANY;
+
+    // creating a TCP socket for our server
+    SOCKET TCPsocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (TCPsocket == INVALID_SOCKET)
+    {
+        std::cout << "SCOKET::CREATION::ERROR" << std::endl;
+        WSACleanup();
+        return 1;
+    }
+
+    // binding the socket with the addr
+    result = bind(TCPsocket, (sockaddr *)&serverAddr, sizeof(serverAddr));
+    if (result == SOCKET_ERROR)
+    {
+        std::cout << "BINDING::ERROR" << std::endl;
+        WSACleanup();
+        return 1;
+    }
+
+    // setting the server to listen
+    result = listen(TCPsocket, SOMAXCONN);
+    if (result == SOCKET_ERROR)
+    {
+        std::cout << "LISTENING::ERROR" << std::endl;
+        WSACleanup();
+        return 1;
+    }
+
+    // accepting client
+    sockaddr_in clientAddr{};
+    int len = sizeof(clientAddr);
+    SOCKET clientSocket = accept(TCPsocket, (sockaddr *)&clientAddr, &len);
+    if (clientSocket == INVALID_SOCKET)
+    {
+        std::cout << "ACCEPTIN::CLIENT::ERROR" << std::endl;
+        WSACleanup();
+        return 1;
+    }
+
+    // recieving message
+    char buffer[1024];
+    result = recv(clientSocket , buffer , sizeof(buffer) , 0);
+    if (result == SOCKET_ERROR){
+        std::cout << "RECV::ERROR" << std::endl;
+        WSACleanup();
+        return 1;
+    }
+
+    // converting the char[n] into a std::string
+    std::string message(buffer , result);
+    std::cout << "client said : " << message << std::endl;
+
+    closesocket(TCPsocket);
+    closesocket(clientSocket);
+
+    WSACleanup();
+
+    return 0;
+}
